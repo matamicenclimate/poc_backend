5c317e46e949606fdb26fd3d50181b947ca56f45
adriandeka <adrian@dekalabs.com>
2022-02-15 09:40:12 +0100

    mailer in carbon-documents collection

--- api/carbon-documents/models/carbon-documents.js
+++ api/carbon-documents/models/carbon-documents.js
@@ -4,12 +4,5 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
-const mailer = require(`${process.cwd()}/utils/mailer`)
 
-module.exports = {
-  lifecycles: {
-    afterCreate: async function (data) {
-      //await mailer.send()
-    }
-  }
-};
+module.exports = {};
