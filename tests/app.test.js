3f1406ec9230df5eac7a5cb1f003d56ad5a47d32
adriandeka <adrian@dekalabs.com>
2022-02-17 09:49:34 +0100

    unnecesary code; timeout fix

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,23 +1,14 @@
 const fs = require('fs');
 const { setupStrapi } = require('./helpers/strapi');
 
+jest.setTimeout(10000);
 
 beforeAll(async () => {
-  jest.setTimeout(20000);
   await setupStrapi();
 });
 
 afterAll(async () => {
-  const dbSettings = strapi.config.get('database.connections.default.settings');
   await strapi.destroy();
-
-  //delete test database after all tests
-  if (dbSettings && dbSettings.filename) {
-    const tmpDbFile = `${__dirname}/../${dbSettings.filename}`;
-    if (fs.existsSync(tmpDbFile)) {
-      fs.unlinkSync(tmpDbFile);
-    }
-  }
 });
 
 require('./integration');
