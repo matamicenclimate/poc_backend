a2f4fab0ec56a194731cee938c6b91f4150877f2
Fernando <fernando.montero.perez@gmail.com>
2022-02-25 11:39:58 +0000

    feat: added algo sdk

--- api/carbon-documents/controllers/carbon-documents.js
+++ api/carbon-documents/controllers/carbon-documents.js
@@ -1,7 +1,8 @@
-'use strict';
+'use strict'
 
 const mailer = require(`${process.cwd()}/utils/mailer`)
 const fileUploader = require(`${process.cwd()}/utils/upload`)
+const { default: algosdk } = require('algosdk')
 
 async function create(ctx) {
   const collectionName = ctx.originalUrl.substring(1)
@@ -14,7 +15,7 @@ async function create(ctx) {
 
   const createdDocument = await strapi.services[collectionName].create(ctx.request.body)
   if (process.env.NODE_ENV === 'test') {
-    return createdDocument  
+    return createdDocument
   }
 
   const url = `${process.env.BASE_URL}${process.env.CONTENT_MANAGER_URL}/${applicationUid}/${createdDocument.id}`
@@ -23,6 +24,16 @@ async function create(ctx) {
   return createdDocument
 }
 
+async function mint() {
+  const token = 'Your algod API token'
+  const server = 'http://127.0.0.1'
+  const port = 8080
+  const client = new algosdk.Algodv2(token, server, port)
+
+  console.log(await client.status().do())
+}
+
 module.exports = {
   create,
+  mint,
 }
