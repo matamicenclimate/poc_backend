66990e6c8e4e021cc93d3833540f491242646fab
Joel García <joel@dekalabs.com>
2022-02-28 12:16:02 +0100

    formatted documents

--- api/carbon-documents/controllers/carbon-documents.js
+++ api/carbon-documents/controllers/carbon-documents.js
@@ -32,12 +32,12 @@ async function mint(ctx) {
 // Function used to wait for a tx confirmation
 async function waitForConfirmation(algodclient, txId) {
   let response = await algodclient.status().do()
-  let lastround = response["last-round"]
+  let lastround = response['last-round']
   while (true) {
     const pendingInfo = await algodclient.pendingTransactionInformation(txId).do()
-    if (pendingInfo["confirmed-round"] !== null && pendingInfo["confirmed-round"] > 0) {
+    if (pendingInfo['confirmed-round'] !== null && pendingInfo['confirmed-round'] > 0) {
       // Got the completed Transaction
-      console.log("Transaction " + txId + " confirmed in round " + pendingInfo["confirmed-round"])
+      console.log('Transaction ' + txId + ' confirmed in round ' + pendingInfo['confirmed-round'])
       break
     }
     lastround++
--- scripts/algorand-create-accounts.js
+++ scripts/algorand-create-accounts.js
@@ -3,10 +3,10 @@ const algosdk = require('algosdk')
 try {
   const acct = algosdk.generateAccount()
   const account1 = acct.addr
-  console.log("Account 1 = " + account1)
+  console.log('Account 1 = ' + account1)
   const account1_mnemonic = algosdk.secretKeyToMnemonic(acct.sk)
-  console.log("Account Mnemonic 1 = " + account1_mnemonic)
-} catch(e) {
+  console.log('Account Mnemonic 1 = ' + account1_mnemonic)
+} catch (e) {
   console.log(e)
 }
 
