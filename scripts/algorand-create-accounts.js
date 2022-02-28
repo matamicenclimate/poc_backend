82ac97074f01a95c25aa5dda8ff8519c52ca7849
Joel García <joel@dekalabs.com>
2022-02-28 11:29:48 +0100

    formatted script

--- scripts/algorand-create-accounts.js
+++ scripts/algorand-create-accounts.js
@@ -1,12 +1,12 @@
-const algosdk = require('algosdk');
+const algosdk = require('algosdk')
 
 try {
-  const acct = algosdk.generateAccount();
-  const account1 = acct.addr;
-  console.log("Account 1 = " + account1);
-  const account1_mnemonic = algosdk.secretKeyToMnemonic(acct.sk);
-  console.log("Account Mnemonic 1 = " + account1_mnemonic);
+  const acct = algosdk.generateAccount()
+  const account1 = acct.addr
+  console.log("Account 1 = " + account1)
+  const account1_mnemonic = algosdk.secretKeyToMnemonic(acct.sk)
+  console.log("Account Mnemonic 1 = " + account1_mnemonic)
 } catch(e) {
-  console.log(e);
+  console.log(e)
 }
 
