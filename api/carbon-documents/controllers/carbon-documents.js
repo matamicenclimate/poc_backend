1c9050cab839c2e716dd1203d81d5c3753cf4753
Fernando Montero <fernando.montero.perez@gmail.com>
2022-03-04 14:34:25 +0000

    chore: mint cleanup (#20)
    * chore: mint cleanup
    
    * fix: fix validation err

--- api/carbon-documents/controllers/carbon-documents.js
+++ api/carbon-documents/controllers/carbon-documents.js
@@ -181,7 +181,7 @@ async function mint(ctx) {
   const { id } = ctx.params
   const carbonDocument = await strapi.services['carbon-documents'].findOne({ id })
   // TODO: remove status != minted. its only here for developers sake
-  if ([!'completed', 'minted'].includes(carbonDocument.status)) {
+  if (!['completed', 'minted'].includes(carbonDocument.status)) {
     return ctx.badRequest('document hasnt been reviewed')
   }
 
