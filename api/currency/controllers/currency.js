3f50dc40e079d440bf68f14a31acee6ad55a286e
Joel García <joel@dekalabs.com>
2022-03-31 08:47:56 +0200

    update currencies logic

--- api/currency/controllers/currency.js
+++ api/currency/controllers/currency.js
@@ -5,4 +5,33 @@
  * to customize this controller
  */
 
-module.exports = {}
+const https = require('https')
+const currencies = require('config').currencies
+
+async function updateCurrencies(ctx) {
+  const url = `${process.env.CURRENCY_API}&symbols=${currencies.join(',')}`
+
+  https.get(url, (res) => {
+    let data = ''
+
+    res.on('data', (chunk) => {
+      data += chunk
+    })
+
+    res.on('end', async () => {
+      const exchange = JSON.parse(data)
+      const currencyData = {}
+
+      currencies.forEach((currency) => {
+        currencyData[`usd_${currency.toLowerCase()}`] = exchange.rates[currency]
+      })
+
+      await strapi.services.currency.createOrUpdate(currencyData)
+      strapi.log.info('[cron] Currencies updated')
+    })
+  })
+}
+
+module.exports = {
+  updateCurrencies,
+}
