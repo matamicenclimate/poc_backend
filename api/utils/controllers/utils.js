1ff403822922c4dbb4f09b3655557ea17f8a6b5d
Joel García <joel@dekalabs.com>
2022-04-21 09:38:12 +0200

    chart data by month or by week

--- api/utils/controllers/utils.js
+++ api/utils/controllers/utils.js
@@ -2,40 +2,34 @@
 
 async function chartBalanceMe(ctx) {
   const { user } = ctx.state
-  const monthNames = getMonthNames()
-  const labels = getLast12Months(monthNames)
+  const { type } = ctx.query
 
-  let fromDate = new Date()
-  fromDate = new Date(fromDate.setMonth(fromDate.getMonth() - 11))
-  fromDate = new Date(fromDate.getFullYear(), fromDate.getMonth(), 1)
-  ctx.query.date_gte = fromDate
+  let date = new Date()
+  date = new Date(date.setMonth(date.getMonth() - 11))
 
-  const userActivities = await strapi.query('activities').model.aggregate([
-    { $match: { date: { $gte: fromDate }, user: user._id } },
-    {
-      $group: {
-        _id: { $dateToString: { format: '%Y-%m', date: '$date' } },
-        income: { $sum: { $cond: [{ $eq: ['$type', 'swap'] }, '$supply', 0] } },
-        expenses: { $sum: { $cond: [{ $eq: ['$type', 'burn'] }, '$supply', 0] } },
-      },
-    },
-    { $sort: { _id: 1 } },
-  ])
+  let labels,
+    group_id_aggregate,
+    data = await getOtherUserActivities(date, user)
 
-  let data = []
-  userActivities.forEach((userActivity) => {
-    const total = userActivity.income - userActivity.expenses
-    const extractedMonth = userActivity._id.split('-')[1]
-    const extractedMonthName = monthNames[Number(extractedMonth) - 1]
-    const monthIndex = labels.indexOf(extractedMonthName)
-    data[monthIndex] = total
-  })
+  if (type === 'weekly') {
+    const currentWeek = getCurrentWeek(new Date())
+    labels = getWeekNumbers(currentWeek)
+    group_id_aggregate = { $week: '$date' }
+  } else {
+    const monthNames = getMonthNames()
+    labels = getLast12Months(monthNames)
+    group_id_aggregate = { $dateToString: { format: '%Y-%m', date: '$date' } }
+  }
+
+  const userActivities = await getUserActivities(date, user, group_id_aggregate)
+
+  data = calculateTotal(userActivities, type, labels, data)
 
   data = Array.from(data, (item) => item || 0)
   data.forEach((d, i) => {
-    if (data[i - 1] === undefined) data[i] = data[i - 1] = 0
+    if (i !== 0) if (data[i - 1] === undefined) data[i] = data[i - 1] = 0
     if (data[i] === 0) data[i] = data[i - 1]
-    if (data[i] !== data[i - 1]) data[i] += data[i - 1]
+    if (i !== 0 && data[i] !== data[i - 1]) data[i] += data[i - 1]
   })
 
   return { labels, data }
@@ -68,6 +62,70 @@ function getLast12Months(monthNames) {
   lastMonths.push(monthNames[today.getMonth()])
 
   return lastMonths
+}
+
+function getCurrentWeek(date) {
+  const firstDayYear = new Date(date.getFullYear(), 0, 1)
+  const numberOfDays = Math.floor((date - firstDayYear) / (24 * 60 * 60 * 1000))
+  const week = Math.ceil((date.getDay() + numberOfDays) / 7)
+
+  return week
+}
+
+function getWeekNumbers(currentWeek) {
+  const weekNumbers = []
+  for (let i = currentWeek + 1; i <= 52; i++) {
+    weekNumbers.push(i)
+  }
+  for (let j = 1; j <= currentWeek; j++) {
+    weekNumbers.push(j)
+  }
+  return weekNumbers
+}
+
+async function getUserActivities(date, user, group_id_aggregate) {
+  return await strapi.query('activities').model.aggregate([
+    { $match: { date: { $gte: date }, user: user._id } },
+    {
+      $group: {
+        _id: group_id_aggregate,
+        income: { $sum: { $cond: [{ $eq: ['$type', 'swap'] }, '$supply', 0] } },
+        expenses: { $sum: { $cond: [{ $eq: ['$type', 'burn'] }, '$supply', 0] } },
+      },
+    },
+    { $sort: { _id: 1 } },
+  ])
+}
+
+function calculateTotal(userActivities, type, labels, data) {
+  userActivities.forEach((userActivity) => {
+    const total = userActivity.income - userActivity.expenses
+    let activityId = type === 'weekly' ? userActivity._id : userActivity._id.split('-')[1]
+    if (!type || type === 'monthly') {
+      activityId = getMonthNames()[Number(activityId) - 1]
+    }
+    const index = labels.indexOf(activityId)
+    data[index] = total
+  })
+
+  return data
+}
+
+async function getOtherUserActivities(toDate, user) {
+  const otherUserActivities = await strapi.query('activities').model.aggregate([
+    { $match: { date: { $lte: toDate }, user: user._id } },
+    {
+      $group: {
+        _id: 'other_user_activities',
+        income: { $sum: { $cond: [{ $eq: ['$type', 'swap'] }, '$supply', 0] } },
+        expenses: { $sum: { $cond: [{ $eq: ['$type', 'burn'] }, '$supply', 0] } },
+      },
+    },
+    { $sort: { _id: 1 } },
+  ])
+  const otherUserActivitiesTotal = otherUserActivities[0].income - otherUserActivities[0].expenses
+
+  return [otherUserActivitiesTotal]
 }
 
 module.exports = {
