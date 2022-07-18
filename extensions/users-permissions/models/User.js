1255994cdd03acbd0258e727b778165433f571f5
fernando-deka <106664005+fernando-deka@users.noreply.github.com>
2022-07-18 20:41:32 +0200

    feat: initial funding (#79)
    * chore: remove unverified nft mint
    
    * chore: update contract abi
    
    * feat: add new burn parameters
    
    * feat: add new fund txn
    
    * feat: search on indexer the burn contract id + store it
    
    * chore: remove receipt nft services
    
    * feat: add approve txn on claim
    
    * feat: add reject flow
    
    * fix: strapi types and content
    
    * fix: search by date order
    
    * feat: documentation
    
    * feat: give 0.1 algos to the user at address creation
    
    Co-authored-by: Fernando Pascual Soriano <fer112233mc@gmail.com>

---
+++ extensions/users-permissions/models/User.js
@@ -0,0 +1,41 @@
+const algosdk = require("algosdk");
+const {algoClient, algoIndexer} = require("../../../config/algorand");
+
+async function fundUser(user, amount) {
+  const algodClient = algoClient()
+  const suggestedParams = await algodClient.getTransactionParams().do()
+  const creator = algosdk.mnemonicToSecretKey(process.env.ALGO_MNEMONIC)
+
+  const minimumFundsPaymentTxn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+    from: creator.addr,
+    to: user.publicAddress,
+    amount: algosdk.algosToMicroalgos(amount),
+    suggestedParams: suggestedParams,
+  })
+
+  const signedTxn = minimumFundsPaymentTxn.signTxn(creator.sk)
+  const txId = minimumFundsPaymentTxn.txID().toString()
+
+  await algodClient.sendRawTransaction(signedTxn).do()
+  await algosdk.waitForConfirmation(algodClient, txId, 3)
+}
+
+module.exports = {
+  lifecycles: {
+    beforeUpdate: async function (params, newUser) {
+      const { _id } = params
+      const oldUser = await strapi.db.query('plugins::users-permissions.user').findOne({_id})
+      if (oldUser.id !== _id) throw new Error(`User not found with id: ${_id}`)
+      if (oldUser.publicAddress !== newUser.publicAddress) {
+        const indexerClient = algoIndexer()
+        try {
+          const userAddress = await indexerClient.lookupAccountByID(newUser.publicAddress).do()
+        } catch (e) {
+          if (e.status !== 404) throw e
+          console.log("Funding new user")
+          await fundUser(newUser, 0.1)
+        }
+      }
+    },
+  },
+}
