9fad6443c2af219e66380eba1b3985d3cbd8fbb6
Sara Martín <saramartin@MacBook-Pro-de-Sara.local>
2022-07-29 12:37:47 +0200

    fix: fix some strings in Carbon document mail

--- api/compensations/models/compensations.js
+++ api/compensations/models/compensations.js
@@ -54,41 +54,22 @@ module.exports = {
 
       const changeListKeys = Object.keys(newCompensation)
       for (const key of changeListKeys) {
-        const isStateChange = key === "state"
-        const isBurnReceiptChange = key === "burn_receipt"
-        const isCompensationNftChange = key === "compensation_nft"
-        const isConsolidationCertificateChange = key === "consolidation_certificate_ipfs_cid"
-        const isRegistryCertificatesChange = key === "registry_certificates"
+        const isStateChange = key === 'state'
+        const isBurnReceiptChange = key === 'burn_receipt'
+        const isCompensationNftChange = key === 'compensation_nft'
+        const isConsolidationCertificateChange = key === 'consolidation_certificate_ipfs_cid'
+        const isRegistryCertificatesChange = key === 'registry_certificates'
         const wasPreviouslyUndefined = !oldCompensation[key]
-        const isNextStateMinted = newCompensation.state === "minted"
-        const currentStateAllowsRegistryCertificatesChanges = !["minted", "claimed", "rejected"].includes(oldCompensation.state)
+        const isNextStateMinted = newCompensation.state === 'minted'
+        const currentStateAllowsRegistryCertificatesChanges = !['minted', 'claimed', 'rejected'].includes(
+          oldCompensation.state,
+        )
 
-        if (isStateChange) continue;
-        if (isBurnReceiptChange && wasPreviouslyUndefined) continue;
-        if (isCompensationNftChange && wasPreviouslyUndefined && isNextStateMinted) continue;
-        if (isConsolidationCertificateChange && wasPreviouslyUndefined && isNextStateMinted) continue;
-        if (isRegistryCertificatesChange && currentStateAllowsRegistryCertificatesChanges) continue;
-
-        // Do not allow any other change
-        delete newCompensation[key]
-      }
-
-      const changeListKeys = Object.keys(newCompensation)
-      for (const key of changeListKeys) {
-        const isStateChange = key === "state"
-        const isBurnReceiptChange = key === "burn_receipt"
-        const isCompensationNftChange = key === "compensation_nft"
-        const isConsolidationCertificateChange = key === "consolidation_certificate_ipfs_cid"
-        const isRegistryCertificatesChange = key === "registry_certificates"
-        const wasPreviouslyUndefined = !oldCompensation[key]
-        const isNextStateMinted = newCompensation.state === "minted"
-        const currentStateAllowsRegistryCertificatesChanges = !["minted", "claimed", "rejected"].includes(oldCompensation.state)
-
-        if (isStateChange) continue;
-        if (isBurnReceiptChange && wasPreviouslyUndefined) continue;
-        if (isCompensationNftChange && wasPreviouslyUndefined && isNextStateMinted) continue;
-        if (isConsolidationCertificateChange && wasPreviouslyUndefined && isNextStateMinted) continue;
-        if (isRegistryCertificatesChange && currentStateAllowsRegistryCertificatesChanges) continue;
+        if (isStateChange) continue
+        if (isBurnReceiptChange && wasPreviouslyUndefined) continue
+        if (isCompensationNftChange && wasPreviouslyUndefined && isNextStateMinted) continue
+        if (isConsolidationCertificateChange && wasPreviouslyUndefined && isNextStateMinted) continue
+        if (isRegistryCertificatesChange && currentStateAllowsRegistryCertificatesChanges) continue
 
         // Do not allow any other change
         delete newCompensation[key]
