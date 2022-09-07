a111ecd59f08af21869614112fe4ff6cf299ca88
Fernando Pascual Soriano <fer112233mc@gmail.com>
2022-09-07 09:00:28 +0200

    fix: move format function to bootstrap

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -77,6 +77,18 @@ const isFirstRun = async () => {
   return !initHasRun
 }
 
+String.prototype.format = function () {
+  // store arguments in an array
+  var args = arguments
+  // use replace to iterate over the string
+  // select the match and check if the related argument is present
+  // if yes, replace the match with the argument
+  return this.replace(/{([0-9]+)}/g, function (match, index) {
+    // check if the argument is present
+    return typeof args[index] == 'undefined' ? match : args[index]
+  })
+}
+
 module.exports = async () => {
   await setDefaultPermissions()
 }
