27f479f08890bfab04e179751ab590d53ab56088
Fernando Pascual Soriano <fer112233mc@gmail.com>
2022-09-09 19:26:26 +0200

    feat: User global variable on tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,14 +1,18 @@
-const fs = require('fs')
 const { setupStrapi, deleteDbUploadFiles } = require('./helpers/strapi')
 const algosdk = require('algosdk')
+const { createAuthenticatedUser, deleteUser } = require('./features/helpers')
 
 jest.setTimeout(10000)
 
 beforeAll(async () => {
   await setupStrapi()
+  const result = await createAuthenticatedUser()
+  user = result.user
+  jwt = result.jwt
 })
 
 afterAll(async () => {
+  await deleteUser(user.id)
   await deleteDbUploadFiles()
   await strapi.destroy()
 })
